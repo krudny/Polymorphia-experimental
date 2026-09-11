d49c05da28f6fba16314bbac0f727bc854575aa8
Kamil Rudny <k.rudny1@gmail.com>
2026-09-11 19:33:01 +0200

    x

---
+++ terraform/storage.tf
@@ -0,0 +1,22 @@
+resource "google_storage_bucket" "seed_bucket" {
+  name                     = "polymorphia-db-seeds-${var.project_id}"
+  location                 = "EU"
+  storage_class            = "STANDARD"
+  public_access_prevention = "enforced"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_storage_bucket_iam_member" "sa_storage_reader" {
+  bucket = google_storage_bucket.seed_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:github-actions-sa@${var.project_id}.iam.gserviceaccount.com"
+}
+
+output "seed_bucket_name" {
+  value       = google_storage_bucket.seed_bucket.name
+  description = "Bucket name for database test data"
+}
